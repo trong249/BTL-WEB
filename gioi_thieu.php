75a1cce4bdb2962faf206f5dc3c9f94ecf3dcfb8
Nam Diep <namdiep239@gmail.com>
2021-10-16 23:41:54 +0700

    create responsive.css and modify php,html,css file

--- gioi_thieu.php
+++ gioi_thieu.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <!-- <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/gioi_thieu/style.css">
@@ -10,7 +10,25 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/8f65ebef09.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/> -->
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <script src="https://kit.fontawesome.com/8f65ebef09.js" crossorigin="anonymous"></script> 
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+    <!-- favicon -->
+    <link rel="icon" href="./img/gioi_thieu/tachnen.png" type="image/x-icon">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/gioi_thieu/style.css">
+    <link rel="stylesheet" href="css/gioi_thieu/responsive.css">
 
     <title>Giới Thiệu</title>
 </head>
@@ -22,9 +40,92 @@
         document.querySelector('.top-menu ul li:nth-child(3) a').classList.add('active')
     </script>
 <!-- -------------------------------------------------------------------------------------------------------------------------------------------- -->
-    
-    
+<section class="main">
+        <div class="slider">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="./img/gioi_thieu/slider5.png" class="d-block w-100" alt="sliderpic">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="./img/gioi_thieu/slider1.png" class="d-block w-100" alt="sliderpic">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="./img/gioi_thieu/slider6.png" class="d-block w-100" alt="sliderpic">
+                  </div>
+                </div>
+              </div>
+        </div>
 
+
+        <div class="content1">
+        
+            <div class="column text">
+                <h1>Về chúng tôi</h1>
+                <h4>Về cái tên "ishin"</h4>
+                <p>"ISHIN" mang ý nghĩa là UY TÍN. Khi chọn cái tên này, chúng tôi muốn đặt
+                    uy tín của cửa hàng lên hàng đầu. Với ISHIN, tạo sự tin tưởng, tin dùng với khách
+                    hàng là điều vô cùng quan trọng. ISHIN luôn mong muốn có thể là một trong
+                    những sự lựa chọn hàng đầu khi khách hàng có nhu cầu về sneaker. ISHIN luôn sẵn
+                    lòng phục vụ bạn khi bạn cần.
+                </p>
+                <p>
+                    Với câu slogan "Get every step with ISHIN", chúng tôi hi vọng có thể được đồng hành
+                    cùng khách hàng trên mỗi chuyến đi, dõi theo từng cung bậc cảm xúc trên từng nẻo đường. Cung cấp cho bạn những mẫu giày phù hợp nhất.
+                </p>
+            </div>
+            <div class="column pic">
+                <img src="./img/gioi_thieu/logo4.png" alt="logo">
+            </div>
+        </div>
+
+        <div class="content2">
+            <div class="column pic">
+                <img src="./img/gioi_thieu/pic1.jpg" alt="picture">
+            </div>
+    
+            <div class="column text">
+                <h3>Về cửa hàng</h3>
+                <p>ISHIN là chuỗi cửa hàng cung cấp các mẫu giày sneaker với hơn 10 chi nhánh phủ sóng trên toàn quốc</p>
+                <p>
+                    Nhập khẩu và cung cấp rất nhiều hãng giày từ nội địa đến quốc tế,
+                    ISHIN muốn mang lại cho bạn sự đa dạng trong lựa chọn.
+                    ISHIN cam kết đem đến cho khách hàng những giá trị tốt nhất với
+                    những mặt hàng chính hãng, chất lượng, giá cả hợp lí, đội ngũ nhân viên
+                    hỗ trợ tư vấn nhiệt tình mỗi khi bạn cần. 
+                </p>
+                <p>
+                    Đồng thời, trong năm ISHIN còn có rất nhiều chương trình khuyến mãi, ưu đãi, quà tặng, voucher...
+                    để phục vụ cho khách hàng. Vì vậy, khách hàng khi đến với ISHIN thì có thể mua sắm
+                    thoải mái mà không cần phải lo nghĩ quá nhiều về giá cả.
+                </p>
+            </div>
+        </div>
+
+        <div class="content3">
+            <div class="column text">
+                <h3>Về phục vụ</h3>
+                <p>
+                    Đặt khách hàng là tiêu chí hàng đầu, ISHIN đồng hành cùng đội ngũ nhân viên đã được đào tạo kĩ lưỡng, luôn niềm nở và trong tâm thế
+                    sẵn sàng phục vụ bất cứ khi nào khách hàng cần.
+                </p>
+                <p>
+                    ISHIN cam kết chất lượng chính hãng, đầy đủ giấy tờ nhập khẩu và kinh doanh hợp pháp. Ngoài ra, ISHIN còn cung cấp dịch vụ gói quà và
+                    ship hàng hỏa tốc. 
+                </p>
+                <p>Choose ISHIN, choose your joy</p>
+            </div>
+            <div class="column pic">
+                <img src="./img/gioi_thieu/logo3.png" alt="logo">
+            </div>
+        </div>
+        
+        <div class="last">
+            <img src="./img/gioi_thieu/end.png" alt="goodbye">
+        </div>
+
+
+    </section>
 <!-- -------------------------------------------------------------------------------------------------------------------------------------------- -->
     <!-- IMPORT FOOTER -->
     <?php require('footer.php') ?>  
