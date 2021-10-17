63bf7ded66c660282578b14c3c1dd6c9244e015a
Nam Diep <namdiep239@gmail.com>
2021-10-17 22:23:25 +0700

    modify php file

--- gioi_thieu.php
+++ gioi_thieu.php
@@ -132,5 +132,7 @@
 <!-- -------------------------------------------------------------------------------------------------------------------------------------------- -->
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script src="js/gioi_thieu/main.js"></script>
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
