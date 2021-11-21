c3df980977da0ca0fa493d2c4b0ed62f5573fb85
Tran Huynh <56352798+jarm237@users.noreply.github.com>
2021-11-21 16:02:42 +0700

    Update don_hang.php

--- admin/don_hang/don_hang.php
+++ admin/don_hang/don_hang.php
@@ -2,6 +2,21 @@
     require_once "../check_admin.php";
 ?>
 
+<?phP
+    $sql=mysqli_connect("localhost","root","","data_ishine");
+/****************************************************************************************/  
+    $selectData = "SELECT ma_don, user, date, tinh_trang FROM don_hang";
+    $row=$sql->query($selectData);
+    $arr_don_hang =array();
+    
+    while($res=$row->fetch_assoc()){
+        array_push($arr_don_hang,$res);
+    }
+    $don_hang=json_encode($arr_don_hang); //doi tu bien php sang bien json
+/****************************************************************************************/
+    $sql->close();
+?>  
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -64,14 +79,13 @@
                 </div>
                 <div>
                     <a href="../../index.php">Back to shop!</a>
-                    <a href="../../user/doi_mat_khau.php" style=" margin-left:10px">Change password!</a>
                 </div>
             </div>
             <div class="content">
                 <p style="margin-bottom: 30px;">Dưới đây là danh sách các đơn hàng:</p>
                 <div>
                     <table>
-                        <tr>
+                        <!--<tr>
                             <th>Mã đơn</th>
                             <th>User</th>
                             <th>Date</th>
@@ -112,7 +126,7 @@
                             <td>14-10-2021</td>
                             <td><span id="status_0" class="status">Đã hủy đơn</span></td>
                             <td><a href="./chi_tiet.php" class="chi_tiet">Chi tiết</a></td>
-                        </tr>
+                        </tr>-->
                         
                     </table>
                 </div>
@@ -121,6 +135,46 @@
             
 <!-- --------------------------------------------------------------------------------------------------------------------------- -->
 <!-- Script for char -->
+    <script>
+        let arr_don_hang=JSON.parse(JSON.stringify(<?php echo $don_hang ?>));
+
+        function tinh_trang_class(a)
+        {
+            if (a == 0)
+                return "status_0";
+            else if (a == 1)
+                return "status_1";
+            else if (a == 2)
+                return "status_2";
+            else if (a == 3)
+                return "status_3";
+            else if (a == 4)
+                return "status_4";
+        }
+
+        function render(){
+            let html=`<tr>
+                            <th>MÃ ĐƠN</th>
+                            <th>USER</th>
+                            <th>DATE</th>
+                            <th>TÌNH TRẠNG</th>
+                            <th colspan="2">OPTION</th>
+                        </tr>`;
+            for(let i=0;i<arr_don_hang.length;i++){
+                const tinhtrang = ["Đã hủy đơn", "Chưa xác nhận", "Đã xác nhận", "Đang giao hàng", "Hoàn thành"];
+                html+=` <tr>
+                        <td>${arr_don_hang[i].ma_don}</td>
+                        <td>${arr_don_hang[i].user}</td>
+                        <td>${arr_don_hang[i].date}</td>
+                        <td><span id="${tinh_trang_class(arr_don_hang[i].tinh_trang)}" class="status">${tinhtrang[arr_don_hang[i].tinh_trang]}</span></td>
+                        <td><a href="./chi_tiet_test.php?id=${arr_don_hang[i].ma_don}" class="chi_tiet">Chi tiết</a></td>
+                    </tr>`;
+
+            }
+            document.querySelector("table").innerHTML=html;
+        }
+        render();
+    </script>
 
 </body>
 </html>
