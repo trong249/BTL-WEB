f1173672c817b2122f66d0a02ef3135084c3d32b
Nam Diep <namdiep239@gmail.com>
2021-11-22 09:42:34 +0700

    admin favicon

--- admin/brand/brand.php
+++ admin/brand/brand.php
@@ -40,7 +40,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-
+    <link rel="icon" href="../../img/tachnen.png" type="image/x-icon">
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap-grid.min.css" /> -->
     <script src="https://kit.fontawesome.com/8f65ebef09.js" crossorigin="anonymous"></script>
 
