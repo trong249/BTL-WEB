5e3640656c5e4bdf9e814750f18a9684896f4b01
Kuzuma-kun <ngoccatt@gmail.com>
2021-11-29 13:51:01 +0700

    forgor - mật khẩu sử dụng app password

--- shop/forgor.php
+++ shop/forgor.php
@@ -121,7 +121,7 @@
                     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                     $mail->Username   = 'satoukazuma.clone@gmail.com';                     //SMTP username
-                    $mail->Password   = 'doraemon12!3';                               //SMTP password
+                    $mail->Password   = 'dhihcsupdexkpqhi';                               //SMTP app password
                     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
                     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
                     $mail->CharSet = 'UTF-8';
